a3f7dbc55ac25446234ee97af9698c37fb236947
naiiiden <na4ko359@gmail.com>
2022-07-03 17:11:44 +0300

    header styled on desktop

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,21 @@
 import { ReactComponent as Monogram } from "../images/monogram.svg";
 import { ReactComponent as Menu} from "../images/menu.svg";
 import { ReactComponent as Cart } from "../images/cart.svg";
+import { useState, useEffect } from "react";
 
 const Header = () => {
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1024);
+
+    const updateMedia = () => setIsDesktop(window.innerWidth >= 1024);
+
+    useEffect(() => {
+        window.addEventListener("resize", updateMedia);
+        return () => window.removeEventListener("resize", updateMedia);
+    });
+
     return (
-        <header>
+        <>
+        {!isDesktop ? (<header>
             <Monogram className="monogram"/>
             <div className="header--buttons--container">
                 <button aria-label="Open menu">
@@ -29,7 +40,36 @@ const Header = () => {
                     <li><a href="#" className="link--bold">SHOP</a></li>
                 </ul>
             </nav>
+        </header>) : (
+            <header>
+            <Monogram className="monogram"/>
+            <div className="header--buttons--container">
+                <button aria-label="Go to checkout">
+                    <Cart className="cart--icon"/>
+                </button>
+            </div>
+            <nav className="header--nav">
+                <ul className="header--nav--ul">
+                    <li><a href="#">HOW IT WORKS</a></li>
+                    <li>
+                        <a href="#">WORKFLOWS</a>
+                        <ul className="header--desktop--dropdown">
+                            <li className="link-padding-left"><a href="#">PHOTO CONSOLE</a></li>
+                            <li className="link-padding-left"><a href="#">VIDEO CONSOLE</a></li>
+                            <li className="link-padding-left"><a href="#">AUDIO CONSOLE</a></li>
+                            <li className="link-padding-left"><a href="#">VIRTUAL PRODUCTION</a></li>
+                            <li className="link-padding-left"><a href="#">MINI CONSOLE</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">DOWNLOAD</a></li>
+                    <li><a href="#">BLOG</a></li>
+                    <li><a href="#">SUPPORT</a></li>
+                    <li><a href="#" className="link--bold">SHOP</a></li>
+                </ul>
+            </nav>
         </header>
+        )}
+        </>
     )
 }
 
