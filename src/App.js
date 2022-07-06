048d4ad56303cbb39a4973e5bf57a78d7de8f9e3
naiiiden <na4ko359@gmail.com>
2022-07-06 14:56:45 +0300

    hero component background now responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./style/general.css";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <div>
       <Header/>
       <main>
-        
+        <Hero h1Text="A CONSOLE FOR EVERY WORKFLOW" pText="Discover the perfect console for yours."/>
       </main>
     </div>
   )
