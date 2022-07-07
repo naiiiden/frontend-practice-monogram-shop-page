d3338c2efc31693c1fb82634318c3d1fb207803b
naiiiden <na4ko359@gmail.com>
2022-07-07 11:43:39 +0300

    product component styled on mobile

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,12 +3,12 @@ const Product = ({ src, preOrder, productName, price, desc, href, onMouseEnter, 
     return (
         <article>
             <a href={href} className="product--container" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-                <div className="img--container" style={{position: "relative"}}>
+                <div className="img--container">
                     <img src={src} alt=""/>
-                    {preOrder ? <div className="pre-order" style={{position: "absolute", top: "0", right: "0"}}>PRE-ORDER</div> : ""}
+                    {preOrder ? <div className="pre-order">PRE-ORDER</div> : ""}
                 </div>
                 <div className="product--desc--container">
-                    <div className="product--price--container" style={{display: "flex", justifyContent: "space-between"}}>
+                    <div className="product--price--container">
                         <h3>{productName}</h3>
                         <div>{price}</div>
                     </div>
