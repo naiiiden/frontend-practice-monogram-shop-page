73bd40712c2f27cf593eb74704cda40f894ea093
naiiiden <na4ko359@gmail.com>
2022-07-10 11:17:36 +0300

    cart 'continue shopping' button styled

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,10 +2,13 @@ import { ReactComponent as Arrow_left } from "../images/arrow_left.svg";
 
 const Cart = ({ onClick }) => {
     return (
-        <section class="cart--modal" style={{background: "red"}}>
-            <button onClick={onClick}>
-                <Arrow_left style={{fill: "#fff"}}/>
-            </button>
+        <section className="cart--modal">
+            <div className="arrow--container">
+                <button onClick={onClick}>
+                    <Arrow_left className="arrow"/>
+                    CONTINUE SHOPPING
+                </button>
+            </div>
         </section>
     )
 }
